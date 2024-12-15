0ea31faa1454df3d38929a4058ea00a8ee659e9e
Miki <90838632+3miki@users.noreply.github.com>
2024-12-15 21:17:19 +0000

    create frontend with next.js

---
+++ frontend/app/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import supabase from "./supabaseClient";
+import { useEffect, useState } from "react";
+import { getConversationalAiSignedUrl } from "./getConversationalAiSignedUrl";
+
+type Question = {
+  id: number,
+  question: string;
+  answer: string;
+  url: string;
+}
+
+const fetchSignedUrl = async () => {
+  const url = await getConversationalAiSignedUrl();
+  console.log('signed url', url)
+}
+
+export const QuizQuestion = ({question}: {question: Question}) => {
+  console.log('question', question)
+  return <div className="flex justify-center relative p-4 m-4 rounded-2xl">
+    <div className="z-10">
+    <Image className="absolute top-0 left-0" src={question.url} alt={`Image for quiz ${question.id}`}  width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} />
+    <div className="flex relative text-green-700 bg-slate-200/80 p-4 m-4 rounded-2xl">
+    <h3 className="text-lg font-bold text-center">Quiz {question.id}: {question.question}</h3>
+    </div>
+    {/* <p>image url: {question.url}</p> */}
+  </div>
+  </div>
+}
+
+
+
+export const QuizAnswer = ({question}: {question: Question}) => {
+  console.log('question', question)
+  return <div>
+    <h3>Quiz {question.id}: {question.answer}</h3>
+  </div>
+}
+
+export default function Home() {
+  // check if quizCode=${} is in the url, if it is, render the quiz page component. Otherwise render the home page component
+
+  // test supabase connection
+  // console.log("superbase", supabase);
+
+  const [fetchError, setFetchError] = useState<string | null>(null);
+  const [quiz, setQuiz] = useState<Question | null>(null);
+  const [quizId, setQuizId] = useState<number | null>(1);
+
+  useEffect(() => {
+    // create async function
+    const quizId = 2;
+    const fetchQuiz = async () => {
+      const { data, error } = await supabase.from("quiz").select().eq('id', quizId);
+      console.log(data)
+      // .select () to get all the data from the table
+      console.log(error)
+      if (error) {
+        setFetchError('Could not fetch quizzes')
+        setQuiz(null)
+        // console.error(error)
+      }
+      if (data){
+        console.log('setting quizzes')
+        setQuiz(data.at(0))
+        setFetchError(null)
+      }
+    }
+
+    fetchQuiz().then(async () => {
+      const url = await getConversationalAiSignedUrl();
+      console.log('signed url', url)
+    });
+  }, []);
+
+  console.log('render', quiz)
+
+  return (
+    <main className="flex flex-col">
+      <h1 className="text-center text-4xl font-bold tracking-tight bg-gradient-to-r from-yellow-500 to-red-600 bg-clip-text text-transparent mt-4">PhoTrivia</h1>
+      {fetchError && <p>{fetchError}</p>}
+      {quiz && (
+
+        // <div className="quiz">
+        //   {quizzes.map((quiz: Question) => (
+        //     <p key={quiz.id}>Quiz {quiz.id}: {quiz.question}</p>
+        //   ))}
+        // </div>
+
+        <QuizQuestion question={quiz} />
+      )}
+            
+      {/* <h2>Ex excepteur consectetur laboris elit esse lorem id sint nulla ullamco nostrud cillum proident fugiat cillum do est cillum laborum officia irure duis excepteur elit eu velit sit do labore duis aliquip culpa veniam enim laboris officia cillum veniam labore tempor consectetur</h2> */}
+    </main>
+  );
+}
