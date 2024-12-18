39fec7237b671ffdd99b6765759a5477b38e1ceb
Miki <90838632+3miki@users.noreply.github.com>
2024-12-18 15:55:53 +0000

    create folder for file upload page

---
+++ frontend/app/fileUpload/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Uploader } from "../fileUpload/fileUpload";
+
+// for uploading images
+const UploaderPage = () => {
+  return (
+    <div>
+      <Uploader />
+    </div>
+  );
+};
+
+export default UploaderPage;
